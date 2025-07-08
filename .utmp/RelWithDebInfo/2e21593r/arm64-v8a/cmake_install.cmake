071abe211af08c7a4acbedabc0c2f5bbe41c3e19
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2025-07-09 05:12:04 +0900

    save

--- .utmp/RelWithDebInfo/2e21593r/arm64-v8a/cmake_install.cmake
+++ .utmp/RelWithDebInfo/2e21593r/arm64-v8a/cmake_install.cmake
@@ -44,6 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
+  include("C:/GithubProjectFile/M-FlySeagull/.utmp/RelWithDebInfo/2e21593r/arm64-v8a/GameActivity/cmake_install.cmake")
   include("C:/GithubProjectFile/M-FlySeagull/.utmp/RelWithDebInfo/2e21593r/arm64-v8a/FramePacing/cmake_install.cmake")
 
 endif()
